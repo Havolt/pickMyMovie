de3a646fbad4d47912411df1933bee640b575ecd
Mark Fitzpatrick <markfitz815@gmail.com>
2018-06-12 20:21:58 +0100

    added functions to give and remove red border class on mouseover/out to userMovieCross

--- alpha/scripts/mainScr.js
+++ alpha/scripts/mainScr.js
@@ -5,15 +5,30 @@ let vm = new Vue({
     el: '#vApp',
     data: {
         test: 'hello world',
+        pageTitle: 'Pick My Movie',
         buttonTxt: 'Add Film',
         userMovies: [],
         userMovieInput: ''
     },
     methods: {
         addFilmFunc: function(){
-            this.userMovies.push(this.userMovieInput);
-            this.userMovieInput = '';
-            console.log(this.userMovies);
+            if(this.userMovieInput.length > 1){
+                this.userMovies.push(this.userMovieInput);
+                this.userMovieInput = '';
+            }
+        },
+        crossMOver: function(e){
+            e.target.parentElement.classList.add('redBorder');
+        },
+        crossMOff: function(e){
+            e.target.parentElement.classList.remove('redBorder');
         }
     }
-})
+});
+
+
+/*
+(function initApp(){
+    
+})()
+*/
